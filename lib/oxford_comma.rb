b32e76d86ebc7ad62ccb97cf3690b8297876c722
Eric J Wilhite <ericjwilhite@gmail.com>
2019-07-25 16:17:59 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
-def oxford_comma(array)
+require "pry"
 
+def oxford_comma(array)
+  counter = 0
+  oxford_string = ""
+  while counter < array.length
+    if counter == 0 
+      oxford_string << "#{array[counter]}"
+    elsif array.length == 2
+      array.join(" and ")
+    elsif counter == array.length - 1
+      oxford_string << ", and #{array[counter]}"
+    else
+      oxford_string << ", #{array[counter]}"
+    end
+    counter += 1
+  end
+  oxford_string
 end
