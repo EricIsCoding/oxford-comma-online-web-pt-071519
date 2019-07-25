2171174757a47b1180b29f2dc96d7405ce140841
Eric J Wilhite <ericjwilhite@gmail.com>
2019-07-25 16:24:49 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
-def oxford_comma(array)
+require "pry"
 
+def oxford_comma(array)
+  counter = 0
+  oxford_string = ""
+  array.each_with_index do |word, index|
+    if index == 0 
+      oxford_string << "#{word}"
+    elsif array.length == 2
+      oxford_string = array.join(" and ")
+    elsif index == array.length - 1
+      oxford_string << ", and #{word}"
+    else
+      oxford_string << ", #{word}"
+    end
+  end
+  oxford_string
 end
